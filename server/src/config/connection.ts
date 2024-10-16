796487b933069cf7c3a5487e6c8795b99ee15c51
Rory Dowse <rorydowse@hotmail.com>
2024-10-15 22:56:16 -0700

    env

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || '';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://rorydowse:Rn3ZI7Loxe94dUta@cluster0.vxnen.mongodb.net/googleBooks?retryWrites=true&w=majority&appName=Cluster0';
 console.log(process.env.MONGODB_URI);
 
 const db = async (): Promise<typeof mongoose.connection> => {
